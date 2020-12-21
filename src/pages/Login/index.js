20e0f979f8edf6f0cd7a517894ecf89efc0257d3
Jonatas Alves <jonatas.a2012@hotmail.com>
2020-12-21 02:26:37 -0300

    Add Edit Geral

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,15 +4,21 @@ import './styles.css';
 function Login() {
   return (
     <div className="login__page">
-      <h1>Login</h1>
-      <p>Por favor, preencha os campos abaixo para entrar em sua conta!</p>
-      <form>
-        <input className="login__input" type="text" placeholder="E-mail"/>
-        <input className="login__input" type="text" placeholder="Senha" />
-        <button className="login__btn">Entrar</button>
-      </form>
+      <div className="login__container__info">
+        <div className="login__container__details">
+          <h1>Login</h1>
+        </div>
+      </div>
+      
+      <div className="login__container__form">
+        <form>
+          <input className="login__input" type="text" placeholder="E-mail"/>
+          <input className="login__input" type="text" placeholder="Senha" />
+          <button className="login__btn">Entrar</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login;
